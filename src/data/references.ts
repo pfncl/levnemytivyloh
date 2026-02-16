77d02d98646b36079fd9267e2612f8ae5793b95c
pfncl <177448622+pfncl@users.noreply.github.com>
2026-02-16 13:01:06 +0100

    chore(references): update details and URLs
    - Update alt texts for several entries
    - Replace placeholder URLs with actual links
    - Provide more specific names for references
    - Ensure all reference entries have valid URLs

--- src/data/references.ts
+++ src/data/references.ts
@@ -7,16 +7,16 @@ export interface Reference {
 }
 
 export const references: Reference[] = [
-  { src: '/images/references/bori.webp', alt: 'Bori', width: 300, height: 199, url: 'https://www.bfranz.cz/' },
-  { src: '/images/references/waltr.webp', alt: 'Waltr', width: 300, height: 217, url: 'https://www.waltr.cz/' },
+  { src: '/images/references/bori.webp', alt: 'Bořislavka Centrum', width: 300, height: 199, url: 'https://kkcg.com/cs/borislavka-centrum' },
+  { src: '/images/references/waltr.webp', alt: 'Nová Waltrovka', width: 300, height: 217, url: 'https://www.novawaltrovka.cz/' },
   { src: '/images/references/airbank.webp', alt: 'Air Bank', width: 300, height: 217, url: 'https://www.airbank.cz/' },
-  { src: '/images/references/logo_black.webp', alt: 'Reference', width: 300, height: 217, url: '#' },
-  { src: '/images/references/ifcoffe.webp', alt: 'IF Coffee', width: 300, height: 217, url: '#' },
-  { src: '/images/references/logo_coloseum.webp', alt: 'Coloseum', width: 302, height: 217, url: '#' },
-  { src: '/images/references/logo_husa.webp', alt: 'Husa', width: 300, height: 217, url: '#' },
+  { src: '/images/references/logo_black.webp', alt: 'Masaryčka Spojuje', width: 300, height: 217, url: 'https://www.masaryckaspojuje.cz/' },
+  { src: '/images/references/ifcoffe.webp', alt: 'IF Coffee', width: 300, height: 217, url: 'https://ifcafe.cz/' },
+  { src: '/images/references/logo_coloseum.webp', alt: 'Pizza Coloseum', width: 302, height: 217, url: 'https://www.pizzacoloseum.cz/' },
+  { src: '/images/references/logo_husa.webp', alt: 'Potrefená Husa', width: 300, height: 217, url: 'http://www.staropramen.cz/husa/' },
   { src: '/images/references/logo_istyle.webp', alt: 'iStyle', width: 302, height: 217, url: 'https://www.istyle.cz/' },
-  { src: '/images/references/modryzub.webp', alt: 'Modrý zub', width: 300, height: 217, url: '#' },
-  { src: '/images/references/slavia.webp', alt: 'Slavia', width: 300, height: 217, url: '#' },
-  { src: '/images/references/together.webp', alt: 'Together', width: 300, height: 217, url: '#' },
-  { src: '/images/references/dhaba.webp', alt: 'Dhaba', width: 300, height: 217, url: '#' },
+  { src: '/images/references/modryzub.webp', alt: 'Modrý zub', width: 300, height: 217, url: 'http://www.modryzub.com/' },
+  { src: '/images/references/slavia.webp', alt: 'kavárna Slavia', width: 300, height: 217, url: 'https://www.cafeslavia.cz/' },
+  { src: '/images/references/together.webp', alt: 'Together Restaurants', width: 300, height: 217, url: 'https://www.tgthr.cz/' },
+  { src: '/images/references/dhaba.webp', alt: 'Dhaba Beas', width: 300, height: 217, url: 'https://dhababeas.cz/' },
 ];
